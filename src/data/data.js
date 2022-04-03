0271bbb23a8d2f740cadd6f18182b62de669583d
Dusan <dusan.chmelko@yahoo.com>
2022-04-03 20:27:40 +0200

    some minor styling changes

--- src/data/data.js
+++ src/data/data.js
@@ -7,7 +7,9 @@ import sibaImg from "../images/siba-shop.png";
 const projects = [
   {
     name: "Online shop ",
-    text: [""],
+    text: [
+      "An e-commerce web app build with React and Material UI with a little touch of SCSS. Products can be browsed by categories or collections, further filtered by tags. They can be added to favourites and to the shopping bag, both of witch are made persistent. To fetch the data Axios is used, Context API for state management and react-router-dom for navigation. This project is deployed on Netlify.",
+    ],
     keyWords: [["React", "Context API", "Axios", "SASS", "Material UI"]],
     gitHubURL: "https://github.com/osmonoh/siba-shop",
     siteURL: "https://siba-shop-app.netlify.app/",
