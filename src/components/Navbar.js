a3acbcdd396bf8b463e9a2d34c16ad68011064f7
Dusan <dusan.chmelko@yahoo.com>
2022-04-08 15:14:10 +0200

    playing with logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import myLogo from "../images/dc_moon_down.png";
+
 const Navbar = () => {
   return (
     <nav className="navbar center-nav">
       <div className="navbar-left">
         <Link to="/">
-          <div className="logo logo-current">
-            {/* <i className="fa fa-code fa-lg" aria-hidden="true"></i>  */}
+          {/* <div className="logo logo-current">
+            <i className="fa fa-code fa-lg" aria-hidden="true"></i> 
             &lt;&#47;&gt;
+          </div> */}
+          <div className="myLogo">
+            <img src={myLogo} alt="logo" />
           </div>
         </Link>
-        <Link to="/">
+        {/* <Link to="/">
           <div className="name">
             <h5 className="name-heading">Dusan Chmelko</h5>
             <p className="name-title">Web Developer</p>
           </div>
-        </Link>
+        </Link> */}
       </div>
       <div className="navbar-right">
         <Link className="navbar-right-link" to="./pages/resume">
